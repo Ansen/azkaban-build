0d61c822269ae0adca17c1768ccc69780ae630d3
An <shen.an89@gmail.com>
2024-01-05 20:31:25 +0800

    fix: version must be string

--- patch.sh
+++ patch.sh
@@ -2,7 +2,7 @@
 
 VERSION=$(cat ../version)
 # https://github.com/azkaban/azkaban/issues/3329
-sed -i "s/rootProject.version/${VERSION}/g" build.gradle
+sed -i "s/rootProject.version/'${VERSION}'/g" build.gradle
 
 # https://github.com/azkaban/azkaban/issues/3310#issuecomment-1878323131
 sed -i 's#https://nodejs.org/dist#https://mirrors.dotsrc.org/nodejs/release#g'  azkaban-web-server/build.gradle
